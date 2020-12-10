d3a59ee3841557822436d422a400a2fde8b5f48d
Murali Krishnan <muralk@amazon.com>
2020-12-10 13:44:08 -0800

    Adding paranthesis matching implementation using python list data structure

--- paranthesisMatching.py
+++ paranthesisMatching.py
@@ -7,6 +7,7 @@ class ParanthesisMatching:
 	def __init__(self):
 		pass
 
+	# implementation using custom Stack class in StackImplentation.py
 	def isMatching(self, exp):
 		if exp == None or len(exp) == 0:
 			return True
@@ -29,3 +30,31 @@ class ParanthesisMatching:
 			return True
 
 		return False
+
+	# Implementation using python List data structure
+	def isMatch(self, exp):
+		if exp == None or len(exp) == 0:
+			return True
+
+		s = []
+		for ch in exp:
+			if ch == '{' or ch == '(' or ch == '[':
+				s.append(ch)
+			else:
+				if len(s) == 0:
+					return False
+				else:
+					top = s[-1]
+					if (ch == ']' and top == '[') or (ch == '}' and top == '{') or (ch == ')' and top == '('):
+						s.pop()
+					else:
+						return False
+		if len(s) == 0:
+			return True
+		return False
+
+
+
+
+
+
