dd513e4d774199d09baeeb86b2d031a5b8dae4be
Murali K <muralik.msr@gmail.com>
2020-12-10 21:08:52 -0800

    dum

---
+++ add_two_numbers.py
@@ -0,0 +1,33 @@
+# Leet code problem: Add Two Numbers
+# Link: https://leetcode.com/problems/add-two-numbers/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        p = l1
+        q = l2 
+        d = sentinal = ListNode(0)
+        carry = 0
+        while p != None or q != None:
+            carry = carry // 10
+            if p != None:
+                carry += p.val
+                p = p.next
+            
+            if q != None:
+                carry += q.val
+                q = q.next
+            
+            d.next = ListNode(carry % 10)
+            d = d.next
+            
+        if carry // 10 > 0:
+            d.next = ListNode(carry // 10)
+        
+        return sentinal.next
+            
