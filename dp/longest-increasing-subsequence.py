73f107de3d77fef0e639a47b640378e32d6b3492
Murali K <muralik.msr@gmail.com>
2020-12-16 22:46:04 -0800

    fixing the LIS

--- dp/longest-increasing-subsequence.py
+++ dp/longest-increasing-subsequence.py
@@ -6,15 +6,22 @@ class LIS:
 		if s == None or len(s) == 0:
 			return 0
 		n = len(s)
-		M = [0 for x in range(n+1)]
+		M = [1 for x in range(n+1)]
 		for i in range(1, n+1):
-			for j in range(1, n+1):
-				if j>i and s[j-1] >= s[i-1]:
-					M[j] = M[j-1] + 1
-		return M[n]
+			for j in range(i+1, n+1):
+				if s[j-1] >= s[i-1]:
+					M[j] = max(M[i] + 1, M[j])
+			print M
+		
+		largest = M[0]
+		for i in range(1, n+1):
+			largest = max(largest, M[i])
+		return largest
+
+
 
 def main():
-	arr = [10, 22, 9, 33, 21, 50, 41, 60] 
+	arr = [10, 22, 9, 3, 21, 10, 1, 0] 
 	print "Length of lis is", LIS().compute(arr)
 
 if __name__ == '__main__':
