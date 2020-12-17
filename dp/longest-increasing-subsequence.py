f6666284a7ec51cd7611fea8eef0a59389a0f99c
Murali K <muralik.msr@gmail.com>
2020-12-17 07:39:02 -0800

    dive deep into lis n lg n based solution

--- dp/longest-increasing-subsequence.py
+++ dp/longest-increasing-subsequence.py
@@ -2,27 +2,51 @@
 # Link: https://www.geeksforgeeks.org/longest-increasing-subsequence-dp-3/
 
 class LIS:
-	def compute(self, s):
-		if s == None or len(s) == 0:
+	def computeN2Solution(self, A):
+		if A == None:
 			return 0
-		n = len(s)
-		M = [1 for x in range(n+1)]
-		for i in range(1, n+1):
-			for j in range(i+1, n+1):
-				if s[j-1] >= s[i-1]:
-					M[j] = max(M[i] + 1, M[j])
-			print M
-		
-		largest = M[0]
-		for i in range(1, n+1):
-			largest = max(largest, M[i])
-		return largest
+		n = len(A)
+		if (n <= 1):
+			return n
+		dp = [1] * n
+		lis = dp[0]
+		for i in range(1, n):
+			for j in range(0,i):
+				if A[i] >= A[j]:
+					dp[i] = max(dp[i], dp[j]+1)
+			# print dp
+			lis = max(lis, dp[i])
+		return lis
 
+	def computeNlogNSolution(self, A):
+		if A == None:
+			return 0
+		n = len(A)
+		if n <= 1:
+			return n
+		tail = [0] * n
+		seq = [-1] * n
+		size = 0
+		for num in A:
+			i, j = 0, size
+			while i<j:
+				m = (i+j)//2
+				if tail[m] < num:
+					i = m+1
+				else:
+					j = m
+			tail[i] = num
+			if i == size or i == size-1:
+				seq[i] = num
+			size = max(size, i+1)
+		print "sequence of lis: ", seq
+		return size
 
 
 def main():
-	arr = [10, 22, 9, 3, 21, 10, 1, 0] 
-	print "Length of lis is", LIS().compute(arr)
+	arr = [10, 1, 9, 3, 21, 10, 2, 20] 
+	print "Length of lis using DP in N^2 complexity is: ", LIS().computeN2Solution(arr)
+	print "Length of lis using DP & binary search in n(lg n) complexity is: ", LIS().computeNlogNSolution(arr)
 
 if __name__ == '__main__':
 	main()
