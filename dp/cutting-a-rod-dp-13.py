10c1a7940f0566eccd04d6dd6804e7cc978b17e5
Murali K <muralik.msr@gmail.com>
2020-12-20 10:54:45 -0800

    fixing code

--- dp/cutting-a-rod-dp-13.py
+++ dp/cutting-a-rod-dp-13.py
@@ -8,17 +8,14 @@ class Solution:
 		l = [i for i in range(1, n+1)]
 		M = [0 for i in range(0, n+1)]
 
-		print('l = ', l)
-		print('M = ', M)
 		for i in range(n):
 			for j in range(1, n+1):
-				if j <= l[i]:
-					M[j] = max(M[j], price[i] + M[j-l[i]])
-			print(M)
+				if l[i] <= j and price[i] + M[j-l[i]] > M[j]:
+					M[j] = price[i] + M[j-l[i]]
 		return M[n]
 
 def main():
-	arr = [1, 5, 8, 9, 10, 15, 11, 20] 
+	arr = [1, 5, 8, 9, 10, 17, 17, 20] 
 	size = len(arr) 
 	print "Maximum Obtainable Value is", Solution().cutRod(arr, size) 
 
