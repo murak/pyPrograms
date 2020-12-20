17f826fc24946738852e2876b5e14d351ad0979b
Murali K <muralik.msr@gmail.com>
2020-12-20 12:13:33 -0800

    fixing the bug

--- dp/longest-common-subsequence.py
+++ dp/longest-common-subsequence.py
@@ -5,14 +5,18 @@ class LCS:
 	def compute(self, a, b):
 		m = len(a)
 		n = len(b)
-		M = [0 for x in range(n+1)]
+		M = [[0 for x in range(n+1)] for y in range(2)]
+		bi = 0
+		print("\n", M)
 		for i in range(1, m+1):
+			bi = i&1
 			for j in range(1, n+1):
 				if a[i-1] == b[j-1]:
-					M[j] = M[j-1] + 1
+					M[bi][j] = M[1-bi][j-1] + 1
 				else:
-					M[j] = max(M[j], M[j-1])
-		return M[n]
+					M[bi][j] = max(M[1-bi][j], M[bi][j-1])
+			print(M[bi])
+		return M[bi][n]
 
 def main():
 	X = "AGGTAB"
